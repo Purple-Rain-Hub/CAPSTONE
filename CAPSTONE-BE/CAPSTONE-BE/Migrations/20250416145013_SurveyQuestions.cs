333559671c43e0a59b6de3ccbe607a95b9642c70
Manuel Palmieri <manuelpalm2002@gmail.com>
2025-05-08 16:46:27 +0200

    update gitignore

---
+++ CAPSTONE-BE/CAPSTONE-BE/Migrations/20250416145013_SurveyQuestions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CAPSTONE_BE.Migrations
+{
+    /// <inheritdoc />
+    public partial class SurveyQuestions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SurveyQuestions",
+                columns: table => new
+                {
+                    Question = table.Column<string>(type: "nvarchar(800)", maxLength: 800, nullable: false),
+                    Answer = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SurveyQuestions", x => new { x.Question, x.Answer });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SurveyQuestions");
+        }
+    }
+}
---
+++ CAPSTONE-BE/CAPSTONE-BE/Migrations/20250430175357_backUserId.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CAPSTONE_BE.Migrations
+{
+    /// <inheritdoc />
+    public partial class backUserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SurveySessions_AspNetUsers_UserEmail",
+                table: "SurveySessions");
+
+            migrationBuilder.RenameColumn(
+                name: "UserEmail",
+                table: "SurveySessions",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SurveySessions_UserEmail",
+                table: "SurveySessions",
+                newName: "IX_SurveySessions_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SurveySessions_AspNetUsers_UserId",
+                table: "SurveySessions",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SurveySessions_AspNetUsers_UserId",
+                table: "SurveySessions");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "SurveySessions",
+                newName: "UserEmail");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SurveySessions_UserId",
+                table: "SurveySessions",
+                newName: "IX_SurveySessions_UserEmail");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SurveySessions_AspNetUsers_UserEmail",
+                table: "SurveySessions",
+                column: "UserEmail",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
